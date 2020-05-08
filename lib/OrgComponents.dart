39239e78eef9be34b1f6ce80ed3c451c1db5ffc0
Alyssa Fusato <a.c.fusato@gmail.com>
2020-05-08 12:23:04 -1000

    Cleaned up timer face layout, added theme colors to slices

--- lib/OrgComponents.dart
+++ lib/OrgComponents.dart
@@ -3,11 +3,21 @@ import 'package:pie_chart/pie_chart.dart';
 import 'timer_face/PieTimer.dart';
 import 'tasks/BottomDrawer.dart';
 import 'timer_face/Util.dart';
+import 'theme.dart';
 import 'TimeKeeper.dart';
 
 // static functions meant to be used across the app, non-static functions are for
 // pages that require the main timer and task drawer (should only be home)
 class OrgComponents extends StatelessWidget {
+  List<Color> colorList = [ //Colors for task slices
+    Color.fromRGBO(238, 93, 93, 1),
+    Color.fromRGBO(111, 89, 171, 1),
+    Color.fromRGBO(51, 130, 209, 1),
+    Color.fromRGBO(50, 196, 196, 1),
+    Color.fromRGBO(227, 134, 106, 1),
+    Color.fromRGBO(188, 103, 134, 1),
+  ];
+
   Widget build(BuildContext context) {
     Duration time = TimeKeeper.of(context).time;
     return Scaffold(
@@ -31,7 +41,7 @@ class OrgComponents extends StatelessWidget {
               'Drawer Header',
             ),
             decoration: BoxDecoration(
-              // color: theme.primaryColor,
+              color: MaterialColor(CustomColor.red[300].value, CustomColor.red),
             ),
           ),
           // items within menu
@@ -53,8 +63,6 @@ class OrgComponents extends StatelessWidget {
     return AppBar(
       title: Text(
         'PieTime',
-        // apply the text themes from ui branch
-        // style: arr.apply()
       ),
       // backgroundColor: theme.primaryColorDark,
       // backgroundColor: theme.accentColor,
@@ -73,35 +81,45 @@ class OrgComponents extends StatelessWidget {
 
   // everything below the app bar on the main page
   Widget generateAppBody(Duration time) {
+    // TODO: Needs to obtain task list info
     Map<String, double> dataMap = new Map();
     dataMap.putIfAbsent("Mobile Apps Mockings", () => 10);
     dataMap.putIfAbsent("Graphic Design Sketch", () => 15);
     dataMap.putIfAbsent("Core 250 RR", () => 10);
+    double padTimer = 8;
+    double timerSidePadding = 20;
 
     return Stack(
       children: <Widget>[
         Container(
-          margin: EdgeInsets.only(top: 90, right: 15, left: 15),
-          child: Image.asset('assets/TickMarks.png'),
-        ),
-        Container(
-            margin: EdgeInsets.only(bottom: 135.0, right: 30, left: 30),
+            margin: EdgeInsets.only(
+                bottom: 135.0, right: timerSidePadding, left: timerSidePadding),
             child: Stack(
               children: <Widget>[
-                // timer circle
-                PieChart(
-                  dataMap: dataMap,
-                  showLegends: false,
-                  // showChartValueLabel: true,
-                  animationDuration: Duration(milliseconds: 0),
-                  initialAngle: 4.713, //If timer moves clockwise
-                  showChartValuesInPercentage: false,
-                  chartValueStyle: defaultChartValueStyle.copyWith(
-                    color: Colors.blueGrey[900].withOpacity(0.9),
-                    fontSize: 20,
-                  ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Image.asset('assets/TickMarks.png'),
                 ),
-                new PieTimer(time), // PIE TIMER
+                Padding(
+                  child: PieChart(
+                    dataMap: dataMap,
+                    showLegends: false,
+                    showChartValueLabel: false,
+                    animationDuration: Duration(milliseconds: 0),
+                    initialAngle: 4.713, //If timer moves clockwise
+                    showChartValuesInPercentage: false,
+                    colorList: colorList,
+                    chartValueStyle: defaultChartValueStyle.copyWith(
+                      color: Colors.blueGrey[900].withOpacity(0.0),
+                      fontSize: 20,
+                    ),
+                  ),
+                  padding: EdgeInsets.all(padTimer),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(padTimer),
+                  child: new PieTimer(time), // PIE TIMER
+                )
               ],
             )),
         new BottomDrawer() // BOTTOM DRAWER
