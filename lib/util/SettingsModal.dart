4a247ac0b88dea8522ca9a43f9ff8ea6ad4638f1
Alyssa Fusato <a.c.fusato@gmail.com>
2020-08-15 18:59:50 -0700

    Add toggle in settings to keep screen awake
    Added wakelock and flutter toast package, created toggle in settings to
    enable and disable wakelock

--- lib/util/SettingsModal.dart
+++ lib/util/SettingsModal.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wakelock/wakelock.dart';
 import 'theme.dart';
 
 class SettingsModal extends StatefulWidget {
@@ -8,13 +9,15 @@ class SettingsModal extends StatefulWidget {
 }
 
 class _SettingsModalState extends State<SettingsModal> {
-    void _changeTheme(BuildContext buildContext, MyThemeKeys key) {
+  void _changeTheme(BuildContext buildContext, MyThemeKeys key) {
     CustomTheme.instanceOf(buildContext).changeTheme(key);
   }
 
-  static final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  static final Future<SharedPreferences> _prefs =
+      SharedPreferences.getInstance();
   Future<bool> _isDarkMode;
   Future<bool> _shouldDelete;
+  Future<bool> _isAwake;
 
   toggleDarkMode(bool value) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -34,6 +37,15 @@ class _SettingsModalState extends State<SettingsModal> {
     });
   }
 
+  toggleAwake(bool value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _isAwake = prefs.setBool("isAwake", value).then((bool success) {
+        return value;
+      });
+    });
+  }
+
   @override
   void initState() {
     super.initState();
@@ -43,69 +55,110 @@ class _SettingsModalState extends State<SettingsModal> {
     _shouldDelete = _prefs.then((SharedPreferences prefs) {
       return prefs.getBool("shouldDelete") ?? true;
     });
+    _isAwake = _prefs.then((SharedPreferences prefs) {
+      return prefs.getBool("isAwake") ?? true;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text("Settings", style: Theme.of(context).textTheme.bodyText1.copyWith(fontSize: 25)),
-      content: Container(
-        width: double.maxFinite,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            // toggle dark mode
-            FutureBuilder<bool>(
-              future: _isDarkMode,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return ListTile();
-                } else {
-                  if (snapshot.hasError) {
-                    return ListTile(title: Text("Error: ${snapshot.error}"));
+        title: Text("Settings",
+            style:
+                Theme.of(context).textTheme.bodyText1.copyWith(fontSize: 25)),
+        content: Container(
+          width: double.maxFinite,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              // toggle dark mode
+              FutureBuilder<bool>(
+                future: _isDarkMode,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return ListTile();
                   } else {
-                    return SwitchListTile(
-                      value: snapshot.data,
-                     title: Text("Dark Mode",
-                    style: Theme.of(context).textTheme.bodyText1.copyWith(fontSize: 15)),
-                    activeColor: Theme.of(context).primaryColorLight,
-                      contentPadding: EdgeInsets.all(0),
-                      onChanged: (value) {
-                        toggleDarkMode(value);
-                         value ? _changeTheme(context, MyThemeKeys.DARK) : _changeTheme(context, MyThemeKeys.LIGHT);
-                      },
-                    );
+                    if (snapshot.hasError) {
+                      return ListTile(title: Text("Error: ${snapshot.error}"));
+                    } else {
+                      return SwitchListTile(
+                        value: snapshot.data,
+                        title: Text("Dark Mode",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText1
+                                .copyWith(fontSize: 15)),
+                        activeColor: Theme.of(context).primaryColorLight,
+                        contentPadding: EdgeInsets.all(0),
+                        onChanged: (value) {
+                          toggleDarkMode(value);
+                          value
+                              ? _changeTheme(context, MyThemeKeys.DARK)
+                              : _changeTheme(context, MyThemeKeys.LIGHT);
+                        },
+                      );
+                    }
                   }
-                }
-              },
-            ),
-            // toggle should delete tasks after 30 days
-            FutureBuilder<bool>(
-              future: _shouldDelete,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return ListTile();
-                } else {
-                  if (snapshot.hasError) {
-                    return ListTile(title: Text("Error: ${snapshot.error}"));
+                },
+              ),
+              // toggle should delete tasks after 30 days
+              FutureBuilder<bool>(
+                future: _shouldDelete,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return ListTile();
                   } else {
-                    return SwitchListTile(
-                      value: snapshot.data,
-                      title: Text("Delete tasks after 30 days",
-                      style: Theme.of(context).textTheme.bodyText1.copyWith(fontSize: 15)),
-                    activeColor: Theme.of(context).primaryColorLight,
-                      contentPadding: EdgeInsets.all(0),
-                      onChanged: (value) {
-                        toggleDelete(value);
-                      },
-                    );
+                    if (snapshot.hasError) {
+                      return ListTile(title: Text("Error: ${snapshot.error}"));
+                    } else {
+                      return SwitchListTile(
+                        value: snapshot.data,
+                        title: Text("Delete tasks after 30 days",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText1
+                                .copyWith(fontSize: 15)),
+                        activeColor: Theme.of(context).primaryColorLight,
+                        contentPadding: EdgeInsets.all(0),
+                        onChanged: (value) {
+                          toggleDelete(value);
+                        },
+                      );
+                    }
                   }
-                }
-              },
-            ),
-          ],
-        ),
-      )
-    );
+                },
+              ),
+              // toggle wakelock
+              FutureBuilder<bool>(
+                future: _isAwake,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return ListTile();
+                  } else {
+                    if (snapshot.hasError) {
+                      return ListTile(title: Text("Error: ${snapshot.error}"));
+                    } else {
+                      return SwitchListTile(
+                        value: snapshot.data,
+                        title: Text("Keep screen on",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText1
+                                .copyWith(fontSize: 15)),
+                        activeColor: Theme.of(context).primaryColorLight,
+                        contentPadding: EdgeInsets.all(0),
+                        onChanged: (value) {
+                          toggleAwake(value);
+                          value ? Wakelock.enable() : Wakelock.disable();
+                        },
+                      );
+                    }
+                  }
+                },
+              ),
+            ],
+          ),
+        )
+      );
   }
 }
